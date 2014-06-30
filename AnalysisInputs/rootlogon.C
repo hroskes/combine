b7468d099b5d5fe4d2d6c420680335f4dffc4c35
Heshy <jroskes1@jhu.edu>
2014-06-30 09:34:01 -0400

    Fix typo

--- AnalysisInputs/rootlogon.C
+++ AnalysisInputs/rootlogon.C
@@ -2,7 +2,7 @@
 // pre-load HiggsCSandWidth for compiled macros, and set CMSSW include path.
 //
 {
-  gSystem->Load("libHiggsHiggs_CS_and_Width.so");
+  gSystem->Load("libHiggs_CS_and_Width.so");
   gROOT->LoadMacro("$CMSSW_BASE/src/Higgs/Higgs_CS_and_Width/include/HiggsCSandWidth.h+");
 //   gSystem->Load("libZZMatrixElementMELA.so");
 //   gROOT->LoadMacro("$CMSSW_BASE/src/ZZMatrixElement/MELA/interface/Mela.h+");
