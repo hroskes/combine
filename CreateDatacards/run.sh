1b8e15812cd4e73bc4fcbb8b66474e8c90cbd86f
Heshy <jroskes1@jhu.edu>
2014-07-07 19:12:33 -0400

    Do both expected and observed scans

--- CreateDatacards/run.sh
+++ CreateDatacards/run.sh
@@ -62,9 +62,27 @@ then
     cat forsshtolxplus.txt | ssh -Y hroskes@lxplus5.cern.ch 
 fi
 
-python make_prop_DCsandWSs.py -t "templates2DHeshy" -a "HeshyTest" -b -m 1D -i SM_inputs_8TeV 2>&1 &&
-python make_prop_DCsandWSs.py -t "templates2DHeshy" -a "HeshyTest" -b -m 1D -i SM_inputs_7TeV 2>&1 &&
-cd cards_HeshyTest/HCG/ &&
+
+mkdir -p cards_HeshyExpected/HCG/91.2/
+mkdir -p cards_HeshyExpected/HCG_XSxBR/91.2/
+
+python make_prop_DCsandWSs.py -t "templates2DHeshy" -a "HeshyExpected" -b -m 1D -i SM_inputs_8TeV 2>&1 &&
+python make_prop_DCsandWSs.py -t "templates2DHeshy" -a "HeshyExpected" -b -m 1D -i SM_inputs_7TeV 2>&1
+if [ $? -ne 0 ]
+then
+    exit
+fi
+
+cp -r cards_HeshyExpected cards_HeshyObserved
+
+cd cards_HeshyExpected/HCG/
+combineCards.py ./91.2/hzz4l_2e2muS_7TeV.txt ./91.2/hzz4l_4muS_7TeV.txt ./91.2/hzz4l_4eS_7TeV.txt ./91.2/hzz4l_2e2muS_8TeV.txt ./91.2/hzz4l_4muS_8TeV.txt ./91.2/hzz4l_4eS_8TeV.txt > hzz4l_allS_78TeV.txt &&
+text2workspace.py -m 91.2 hzz4l_allS_78TeV.txt -P HiggsAnalysis.CombinedLimit.SpinZeroStructure:spinZeroHiggs -o hzz4l_allS_fZ4l_78TeV.root --PO='' &&
+combine -M MultiDimFit hzz4l_allS_fZ4l_78TeV.root --algo=grid --points 200 -S 1 -m 91.2 -n obs_1D_fL1_PMF -v 3 -t -1 --expectSignal=1 --saveNLL --setPhysicsModelParameters r=1,CMS_zz4l_fai1=0 | tee log_exp_1D_fL1_PMF.log
+cd - > /dev/null
+
+cd cards_HeshyObserved/HCG/
 combineCards.py ./91.2/hzz4l_2e2muS_7TeV.txt ./91.2/hzz4l_4muS_7TeV.txt ./91.2/hzz4l_4eS_7TeV.txt ./91.2/hzz4l_2e2muS_8TeV.txt ./91.2/hzz4l_4muS_8TeV.txt ./91.2/hzz4l_4eS_8TeV.txt > hzz4l_allS_78TeV.txt &&
 text2workspace.py -m 91.2 hzz4l_allS_78TeV.txt -P HiggsAnalysis.CombinedLimit.SpinZeroStructure:spinZeroHiggs -o hzz4l_allS_fZ4l_78TeV.root --PO='' &&
 combine -M MultiDimFit hzz4l_allS_fZ4l_78TeV.root --algo=grid --points 200 -S 1 -m 91.2 -n obs_1D_fL1_PMF -v 3 --saveNLL --setPhysicsModelParameters r=1,CMS_zz4l_fai1=0 | tee log_obs_1D_fL1_PMF.log
+cd - > /dev/null
